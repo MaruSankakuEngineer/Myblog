d23dc5fab182a229d4bc7e7c33d3a3c74f6a5d4b
MaruShunMaru <marusyunmaru@gmail.com>
2024-12-28 18:51:41 +0900

    リッチテキスト方式での投稿に対応

--- Myblog/website/models.py
+++ Myblog/website/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField  # リッチテキスト用フィールド
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200, verbose_name="タイトル")
+    content = RichTextField(verbose_name="内容")  # リッチテキストフィールド
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
 
     def __str__(self):
         return self.title
--- Myblog/website/views.py
+++ Myblog/website/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Article
 
 def home(request):
-    articles = Article.objects.all()  # 記事データを取得
+    articles = Article.objects.all().order_by('-created_at')  # 最新順に取得
     return render(request, 'website/home.html', {'articles': articles})
+

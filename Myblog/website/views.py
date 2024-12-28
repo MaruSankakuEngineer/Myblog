5af3e1c7f9a9fbfa21c4c3ec2101f3bc8a88734e
MaruShunMaru <marusyunmaru@gmail.com>
2024-12-28 20:37:43 +0900

    詳細ページを追加

--- Myblog/website/views.py
+++ Myblog/website/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from .models import Article
+from django.core.paginator import Paginator
+from .models import Article  # Post を Article に変更
 
-def home(request):
+def post_list(request):
     articles = Article.objects.all().order_by('-created_at')  # 最新順に取得
-    return render(request, 'website/home.html', {'articles': articles})
+    paginator = Paginator(articles, 5)  # ページネーションを適用
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'website/post_list.html', {'page_obj': page_obj})
 
-def abount(request):
+def post_detail(request, pk):
+    # ArticleをID（pk）で取得
+    post = Article.objects.get(pk=pk)  # Post → Articleに修正
+    return render(request, 'website/post_detail.html', {'post': post})
+
+
+def about(request):
+    # Aboutページを残す場合はこちらを保持
     return render(request, 'website/about.html')
